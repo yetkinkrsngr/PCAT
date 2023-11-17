d61d7305b8df0d108f46cbcd1301cabc76c87c4f
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-11-17 21:05:42 +0300

    nodemon and express has benn cretat it

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('hello');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'photo1',
+    description: 'this is photo1',
+  };
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
